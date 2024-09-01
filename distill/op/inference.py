4be8f44374f3ca2d416104fcdbf7d8b7531b1704
ruoyuwang1995ucas <ruoyuwang1995@gmail.com>
2024-09-01 15:07:30 +0800

    fix: fix RMSE force bug

--- distill/op/inference.py
+++ distill/op/inference.py
@@ -309,7 +309,7 @@ def get_rmse(
     num_data= len(test_arr)
     if num_data != len(orig_arr):
         raise RuntimeError("Two arrays must be of the same size")
-    return (np.sum(abs(test_arr-orig_arr)**2))**0.5/num_data
+    return (np.sum(abs(test_arr-orig_arr)**2)/num_data)**0.5
                   
             
     
