a03803091671604d240c677d7e1d079963027683
ruoyuwang1995ucas <ruoyuwang1995@gmail.com>
2024-09-27 22:09:05 +0800

    fix: issue with test

--- tests/test_collect_op.py
+++ tests/test_collect_op.py
@@ -1,17 +1,12 @@
 import unittest
 from distill.op.collect import CollectData
-from dflow.python import (
-    OPIO
-)
+from dflow.python import OPIO
 from fake_data_set import (
     fake_multi_sys,
     fake_system,
 )
 from pathlib import Path
 import dpdata
-
-
-
 
 
 class TestCollectData(unittest.TestCase):
@@ -26,8 +21,8 @@ class TestCollectData(unittest.TestCase):
         ss_0 = fake_system(self.nframes_0, self.natoms_0, self.atom_name)
         ss_1 = fake_system(self.nframes_1, self.natoms_1, self.atom_name)
         # dump
-        ss_0.to("deepmd/npy",self.labeled_data[0])
-        ss_1.to("deepmd/npy",self.labeled_data[1])
+        ss_0.to("deepmd/npy", "data0")
+        ss_1.to("deepmd/npy", "data1")
 
     def test_collect(self):
         op = CollectData()
@@ -36,15 +31,15 @@ class TestCollectData(unittest.TestCase):
                 {
                     "systems": self.labeled_data,
                     "type_map": None,
-                    "optional_parameters":{}
+                    "optional_parameters": {},
                 }
             )
         )
-        ss_0=dpdata.System(out["systems"][0],"deepmd/npy")
-        self.assertEqual(ss_0.get_nframes(),6)
-        ss_1=dpdata.System(out["systems"][1],"deepmd/npy")
-        self.assertEqual(ss_1.get_nframes(),5)
-        
-if __name__ == '__main__':
+        ss_0 = dpdata.System(out["systems"][0], "deepmd/npy")
+        self.assertEqual(ss_0.get_nframes(), 6)
+        ss_1 = dpdata.System(out["systems"][1], "deepmd/npy")
+        self.assertEqual(ss_1.get_nframes(), 5)
+
+
+if __name__ == "__main__":
     unittest.main()
-    
